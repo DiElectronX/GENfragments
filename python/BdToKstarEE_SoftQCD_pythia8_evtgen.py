4da433573b2486cd9b05fc40bddf735cb83f2c02
bainbrid <bainbrid@users.noreply.github.com>
2023-01-12 09:13:46 +0100

    removed custom config for pythia8CP5Settings and pythia8CommonSettings

--- python/BdToKstarEE_SoftQCD_pythia8_evtgen.py
+++ python/BdToKstarEE_SoftQCD_pythia8_evtgen.py
@@ -44,8 +44,8 @@ End
         parameterSets = cms.vstring('EvtGen130')
     ),
     PythiaParameters = cms.PSet(
-        #pythia8CommonSettingsBlock,
-        #pythia8CP5SettingsBlock,
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
         #pythia8PSweightsSettingsBlock,
         processParameters = cms.vstring(
             'SoftQCD:nonDiffractive = on',
@@ -56,37 +56,9 @@ End
             'pythia8CommonSettings', 
             'pythia8CP5Settings',
             #'pythia8PSweightsSettings',
-            'processParameters'),
-        pythia8CP5Settings = cms.vstring(
-            'Tune:pp 14', 
-            'Tune:ee 7', 
-            'MultipartonInteractions:ecmPow=0.03344', 
-            'PDF:pSet=20', 
-            'MultipartonInteractions:bProfile=2', 
-            'MultipartonInteractions:pT0Ref=1.41', 
-            'MultipartonInteractions:coreRadius=0.7634', 
-            'MultipartonInteractions:coreFraction=0.63', 
-            'ColourReconnection:range=5.176', 
-            'SigmaTotal:zeroAXB=off', 
-            'SpaceShower:alphaSorder=2', 
-            'SpaceShower:alphaSvalue=0.118', 
-            'SigmaProcess:alphaSvalue=0.118', 
-            'SigmaProcess:alphaSorder=2', 
-            'MultipartonInteractions:alphaSvalue=0.118', 
-            'MultipartonInteractions:alphaSorder=2', 
-            'TimeShower:alphaSorder=2', 
-            'TimeShower:alphaSvalue=0.118'),
-        pythia8CommonSettings = cms.vstring(
-            'Tune:preferLHAPDF = 2', 
-            'Main:timesAllowErrors = 10000', 
-            'Check:epTolErr = 0.01', 
-            'Beams:setProductionScalesFromLHEF = off', 
-            'SLHA:keepSM = on', 
-            'SLHA:minMassSM = 1000.', 
-            'ParticleDecays:limitTau0 = on', 
-            'ParticleDecays:tau0Max = 10', 
-            'ParticleDecays:allowPhotonRadiation = on')
-    ),
+            'processParameters'
+        ),
+    )
 )
 
 ###### Filters ##########
